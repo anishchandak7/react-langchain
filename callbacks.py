2ea34b858772c59e18900b37cea7a1d5d6ee287b
Anish Chandak <chandakanish7@gmail.com>
2024-06-09 16:47:52 +0530

    "Added docstring to AgentCallbackHandler class and module"

--- callbacks.py
+++ callbacks.py
@@ -1,11 +1,15 @@
+"""This module contains a python class `AgentCallbackHandler` which extends `BaseCallbackHandler`
+and defines methods to handle events when an LLM (Language Model) starts and ends running."""
 from typing import Any, Dict, List
 from uuid import UUID
 from langchain.callbacks.base import BaseCallbackHandler
 from langchain_core.outputs import LLMResult
 
 
-class AgentCallbackHandler(BaseCallbackHandler):
 
+class AgentCallbackHandler(BaseCallbackHandler):
+    """This Python class `AgentCallbackHandler` defines methods to handle events when a Language Model
+    (LLM) starts and ends running."""
     def on_llm_start(
         self,
         serialized: Dict[str, Any],
